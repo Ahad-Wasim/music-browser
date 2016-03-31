faf71827d5fb04d6086f5ae000e411abe964411b
Ahad Wasim <ahadwasim@gmail.com>
2016-03-31 15:56:44 -0700

    add scripts, sass, and serve task

--- Ahad-Wasim/gulpfile.js
+++ Ahad-Wasim/gulpfile.js
@@ -1,23 +1,57 @@
+/*
+  Todo:
+  What does Sourcemaps.write do?
+  Hook Up browserSync!
+  Check if all files are working and doing what their supposed to do!
+*/
+
 "use strict"
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const babel = require('gulp-babel');
-const concat = require('concat');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
+const concatOrder = ['app/js', 'app/js']
 
-gulp.task('default', function(){
-  return gulp.src('src/app.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist'))
-})
+gulp.task('scripts', function(){
+  return gulp.src(concatOrder)
+    .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(concat('main.min.js'))
+      .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('Ahad-Wasim/dist/js'))
+    .pipe(browserSync.reload({ stream: true}))
+});
 
 gulp.task('sass', function(){
-  return gulp.src('')
-    .pipe(sass())
-    .pipe(gulp.dest('Ahad-Wasim/dist'))
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(cssnano())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('Ahad-Wasim/dist/css/main.min.css'))
+    .pipe(browserSync.reload({ stream: true }))
 })
 
-gulp.task('', function(){
+gulp.task('serve', function(){
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
+});
 
-})
+gulp.task('watch', ['serve'], function(){
+  gulp.watch('assets/sass/**/*.scss', ['sass'])
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+
+
+
+
