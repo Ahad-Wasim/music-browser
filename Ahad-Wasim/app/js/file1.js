91600dd4abcf3f9f581424ed7d184d9eba0d6c89
Ahad Wasim <ahadwasim@gmail.com>
2016-04-01 17:14:11 -0700

    Added stub JSON data to index Javascript file

--- Ahad-Wasim/app/js/file1.js
+++ Ahad-Wasim/app/js/file1.js
@@ -1,6 +1,68 @@
+// Usually we have to make a ajax request to an API to fetch data but for this case it is all listed out
+let trendingSongsJSON = {
+  songs:[
+    {
+      artist:"Justin Bieber",
+      song: "Love Yourself",
+      album: "Purpose(Deluxe)",
+      albumArtwork:"http://a3.mzstatic.com/us/r30/Music6/v4/0e/38/b4/0e38b405-ae2c-1da4-5959-866f7abb110e/cover100x100.jpeg",
+      rank:"1"
+    },
+    { 
+      artist:"Adele",
+      song: "Hello",
+      album: "25",
+      albumArtwork:"http://a4.mzstatic.com/us/r30/Music6/v4/8c/91/5d/8c915d9b-d9e4-f735-1b91-81ca1b6e6312/cover100x100.jpeg",
+      rank:"2"
+    },
+    { 
+      artist:"Justin Bieber",
+      song: "Sorry",
+      album: "Purpose(Deluxe)",
+      albumArtwork:"http://a3.mzstatic.com/us/r30/Music6/v4/0e/38/b4/0e38b405-ae2c-1da4-5959-866f7abb110e/cover100x100.jpeg",
+      rank:"3"
+    },
+    { 
+      artist:"twenty one pilots",
+      song: "Stressed Out",
+      album: "Blurryface",
+      albumArtwork:"http://a4.mzstatic.com/eu/r30/Music1/v4/05/40/77/05407725-c184-bbc2-c9b0-90f1832adfd7/cover100x100.jpeg",
+      rank:"4",
+    },
+    { 
+      artist:"Roses (feat.ROZES)",
+      song: "The Chainsmokers",
+      album: "Bouquet",
+      albumArtwork:"http://a2.mzstatic.com/us/r30/Music6/v4/dc/88/a0/dc88a036-ebc3-a0ff-fc97-cc5c8026ee7a/cover100x100.jpeg",
+      rank:"5"
+    },
+    { 
+      artist:"Selena Gomez",
+      song: "Same Old Love",
+      album: "Revival(Deluxe)",
+      albumArtwork:"http://a1.mzstatic.com/us/r30/Music3/v4/ea/6d/61/ea6d61ac-0223-39ae-8a83-622821568144/cover100x100.jpeg",
+      rank:"6"
+    },
+    { 
+      artist:"Rachel Platten",
+      song: "Stand By You",
+      album: "Wildfire",
+      albumArtwork:"http://a3.mzstatic.com/us/r30/Music7/v4/8b/72/67/8b726780-c019-62e4-23f5-ddad13f9c593/cover100x100.jpeg",
+      rank:"7"
+    }
+  ]
+}
+
 (function(window, $){
 
-  
+  /*
+    Image URL
+    Image alt tags
+    
+    Song, Artist, Album, Image:URL 
+  */
+
+
   let name = 'hello';
 
 
